5b8449223f9cb763c7ec1460c027711916cf759f
yaxing.jiao <yaxing.jiao@wezebra.com>
2016-09-07 19:31:42 +0800

    use logback to replace log4j

--- src/main/java/star/spring/data/DataController.java
+++ src/main/java/star/spring/data/DataController.java
@@ -1,5 +1,7 @@
 package star.spring.data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.MatrixVariable;
@@ -11,18 +13,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/data")
 public class DataController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(DataController.class);
+	
 	@Value("${data}")
 	String data;
 	
 	@RequestMapping("/pets/{pid}")
 	public @ResponseBody String testMatrixVariable(@PathVariable String pid, @MatrixVariable String q){
-		
 		return "path:" + pid + " MatrixVariable:" + q;
 	}
 	
 	@RequestMapping("/config")
 	public @ResponseBody String getConfig(){
-		
+		logger.debug("data:" + data);
+		logger.error("error test");
 		return data;
 	}
 	
